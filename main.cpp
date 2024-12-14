8eef94f227f1fcd8603d5a7c524cffd6f67aec82
QIZIR1 <riziqrizwan2@gmail.com>
2024-12-14 15:31:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,61 +3,73 @@
 
 using namespace std;
 
-int main()
-{
-    Graph graph;
+int main() {
+    graph G;
+    G.firstVertex = NULL; // Inisialisasi awal
+
     int choice;
     string start, end;
-    int weight;
 
-    switch (choice) {
-        case 1:
-            cout << "Masukkan ID Gudang: ";
-            cin >> start;
-            graph.addVertex(start);
-            break;
-        case 2:
-            cout << "Masukkan Gudang Asal: ";
-            cin >> start;
-            cout << "Masukkan Gudang Tujuan: ";
-            cin >> end;
-            cout << "Masukkan Bobot: ";
-            cin >> weight;
-            graph.addEdge(start, end, weight);
-            break;
-        case 3:
-            cout << "Masukkan ID Gudang yang akan dihapus: ";
-            cin >> start;
-            graph.removeVertex(start);
-            break;
-        case 4:
-            cout << "Masukkan Gudang Asal: ";
-            cin >> start;
-            cout << "Masukkan Gudang Tujuan: ";
-            cin >> end;
-            graph.removeRoute(start, end);
-            break;
-        case 5:
-            cout << "Masukkan Gudang Asal: ";
-            cin >> start;
-            cout << "Masukkan Gudang Tujuan: ";
-            cin >> end;
-            weight = graph.shortPashSearching(start, end);
-            if (weight != -1)
-                cout << "Jarak terpendek dari " << start << " ke " << end << " adalah " << weight << ".\n";
-            break;
-        case 6:
-            cout << "Masukkan Gudang Asal: ";
-            cin >> start;
-            cout << "Masukkan Gudang Tujuan: ";
-            cin >> end;
-            graph.findAlternativeRoutes(start, end);
-            break;
-        case 7:
-            cout << "Keluar dari program. Terima kasih!\n";
-            return 0;
-        default:
-            cout << "Pilihan tidak valid. Coba lagi.\n";
+    do {
+        menu();
+        cout << "Masukkan pilihan: ";
+        while (!(cin >> choice)) {
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+            cout << "Input tidak valid. Masukkan angka: ";
+        }
+
+        switch (choice) {
+            case 1:
+                cout << "Masukkan ID Gudang: ";
+                cin >> start;
+                addVertex(G, start);
+                break;
+            case 2: {
+                int weight;
+                cout << "Masukkan Gudang Asal: ";
+                cin >> start;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> end;
+                cout << "Masukkan Bobot: ";
+                cin >> weight;
+                addEdge(G, start, end, weight);
+                break;
+            }
+            case 3:
+                cout << "Masukkan ID Gudang yang akan dihapus: ";
+                cin >> start;
+                removeVertex(G, start);
+                break;
+            case 4:
+                cout << "Masukkan Gudang Asal: ";
+                cin >> start;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> end;
+                removeRoute(G, start, end);
+                break;
+            case 5: {
+                int weight;
+                cout << "Masukkan Gudang Asal: ";
+                cin >> start;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> end;
+                weight = shortPathSearching(G, start, end);
+                if (weight != -1)
+                    cout << "Jarak terpendek dari " << start << " ke " << end << " adalah " << weight << ".\n";
+                break;
+            }
+            case 6:
+                cout << "Fitur belum tersedia.\n";
+                break;
+            case 7:
+                cout << "Keluar dari program. Terima kasih!\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid. Coba lagi.\n";
         }
         cout << endl;
+    } while (choice != 7);
+
+    return 0;
 }
