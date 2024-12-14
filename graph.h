8b981b04e64424cc0589e63824b41aaf207617c0
QIZIR1 <riziqrizwan2@gmail.com>
2024-12-14 15:30:46 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,20 +1,22 @@
 #ifndef GRAPH_H_INCLUDED
 #define GRAPH_H_INCLUDED
 #include <iostream>
-#define firstVertex(G) G.firstVertex
-#define nextVertex(V) V->nextVertex
-#define infoVertex(V) V->infoVertex
-#define firstEdge(V) V->firstEdge
-#define destvertexID(V) V->destvertexID
-#define nextEdge(E) E->nextEdge
-#define weight(E) E->weight
+#include <climits>
+#include <string>
+#define firstVertex(G) (G).firstVertex
+#define firstEdge(V) (V)->firstEdge
+#define nextVertex(V) (V)->nextVertex
+#define infoVertex(V) (V)->idVertex
+#define destVertexID(E) (E)->destVertexID
+#define nextEdge(E) (E)->nextEdge
+
 using namespace std;
 
 typedef struct vertex *adrVertex;
 typedef struct edge *adrEdge;
 
 struct vertex {
-    string infoVertex;
+    string idVertex;
     adrVertex nextVertex;
     adrEdge firstEdge;
 };
@@ -29,19 +31,20 @@ struct graph {
     adrVertex firstVertex;
 };
 
-// Deklarasi fungsi
+
 void menu();
 adrVertex createVertex(char newVertexID);
 void initGraph(graph *G);
 void buildGraph(graph *G);
 void printGraph(graph *G);
 adrEdge createEdge(string destVertexId, int weight);
-void addVertex(graph &G, char newVertexID);
-void addEdge(graph &G, string searchVertexID )
+void addVertex(graph &G, string newVertexID);
+void addEdge(graph &G, string searchVertexID, string destVertexID, int weight);
 int shortPathSearching(graph &G, string startVertexID, string endVertexID);
 int longPathSearching(graph &G, string startVertexID, string endVertexID);
 void removeRoute(graph &G, string startVertexID, string endVertexID);
 void removeVertex(graph &G, string idVertex);
 void findAlternatifRoutes(graph &G, string startVertexID, string endVertexID);
+
 
 #endif // GRAPH_H_INCLUDED
