9b5a9b3171da60d2e3b2056862e7de98066e3675
tifunnyy <159507386+Funny-pixel765@users.noreply.github.com>
2024-12-26 16:21:32 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,17 +1,16 @@
 #ifndef GRAPH_H_INCLUDED
 #define GRAPH_H_INCLUDED
 #include <iostream>
-#include <climits>
-#include <string>
 #define firstVertex(G) (G).firstVertex
 #define firstEdge(V) (V)->firstEdge
 #define nextVertex(V) (V)->nextVertex
 #define infoVertex(V) (V)->idVertex
 #define destVertexID(E) (E)->destVertexID
 #define nextEdge(E) (E)->nextEdge
-
+#define weight(E) (E)->weight
 using namespace std;
 
+// Definisi tipe data
 typedef struct vertex *adrVertex;
 typedef struct edge *adrEdge;
 
@@ -32,11 +31,9 @@ struct graph {
 };
 
 
+// Deklarasi fungsi
 void menu();
 adrVertex createVertex(char newVertexID);
-void initGraph(graph *G);
-void buildGraph(graph *G);
-void printGraph(graph *G);
 adrEdge createEdge(string destVertexId, int weight);
 void addVertex(graph &G, string newVertexID);
 void addEdge(graph &G, string searchVertexID, string destVertexID, int weight);
@@ -44,7 +41,8 @@ int shortPathSearching(graph &G, string startVertexID, string endVertexID);
 int longPathSearching(graph &G, string startVertexID, string endVertexID);
 void removeRoute(graph &G, string startVertexID, string endVertexID);
 void removeVertex(graph &G, string idVertex);
-void findAlternatifRoutes(graph &G, string startVertexID, string endVertexID);
-
+void showVertex(graph G);
+void showEdge(graph G);
+void showAll(graph G);
 
 #endif // GRAPH_H_INCLUDED
