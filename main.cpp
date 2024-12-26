e4b6240c7740b867aa8a88f35538f47f29e926a1
tifunnyy <159507386+Funny-pixel765@users.noreply.github.com>
2024-12-26 16:20:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "graph.h"
 
@@ -6,6 +7,25 @@ using namespace std;
 int main() {
     graph G;
     G.firstVertex = NULL; // Inisialisasi awal
+    addVertex(G, "GudangA");
+    addVertex(G, "GudangB");
+    addVertex(G, "GudangC");
+    addVertex(G, "GudangD");
+    addVertex(G, "GudangE");
+    addVertex(G, "GudangF");
+    addVertex(G, "GudangG");
+    addVertex(G, "GudangH");
+    addVertex(G, "GudangI");
+
+    addEdge(G,"GudangA","GudangB",5);
+    addEdge(G,"GudangB","GudangD",8);
+    addEdge(G,"GudangD","GudangC",12);
+    addEdge(G,"GudangC","GudangH",20);
+    addEdge(G,"GudangE","GudangA",2);
+    addEdge(G,"GudangG","GudangE",18);
+    addEdge(G,"GudangI","GudangB",11);
+    addEdge(G,"GudangF","GudangH",3);
+    addEdge(G,"GudangE","GudangI",28);
 
     int choice;
     string start, end;
@@ -21,55 +41,84 @@ int main() {
 
         switch (choice) {
             case 1:
-                cout << "Masukkan ID Gudang: ";
-                cin >> start;
-                addVertex(G, start);
+                showVertex(G);
                 break;
-            case 2: {
-                int weight;
-                cout << "Masukkan Gudang Asal: ";
-                cin >> start;
-                cout << "Masukkan Gudang Tujuan: ";
-                cin >> end;
-                cout << "Masukkan Bobot: ";
-                cin >> weight;
-                addEdge(G, start, end, weight);
+            case 2:
+                showEdge(G);
+                break;
+            case 3:
+                showAll(G);
+                break;
+            case 4: {
+                string gudang;
+                cout << "Masukan nama gudang yang akan ditambah: ";
+                cin >> gudang;
+                addVertex(G,gudang);
                 break;
             }
-            case 3:
-                cout << "Masukkan ID Gudang yang akan dihapus: ";
-                cin >> start;
-                removeVertex(G, start);
+            case 5:{
+                string gudangAsal;
+                string gudangTujuan;
+                int jarak;
+                cout << "Masukkan Gudang Asal: ";
+                cin >> gudangAsal;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> gudangTujuan;
+                cout << "Masukan jarak:";
+                cin >> jarak;
+                addEdge(G,gudangAsal,gudangTujuan,jarak);
                 break;
-            case 4:
+            }
+            case 6: {
                 cout << "Masukkan Gudang Asal: ";
                 cin >> start;
                 cout << "Masukkan Gudang Tujuan: ";
                 cin >> end;
                 removeRoute(G, start, end);
                 break;
-            case 5: {
-                int weight;
+            }
+            case 7: {
                 cout << "Masukkan Gudang Asal: ";
                 cin >> start;
                 cout << "Masukkan Gudang Tujuan: ";
                 cin >> end;
-                weight = shortPathSearching(G, start, end);
-                if (weight != -1)
-                    cout << "Jarak terpendek dari " << start << " ke " << end << " adalah " << weight << ".\n";
+                removeRoute(G, start, end);
                 break;
             }
-            case 6:
-                cout << "Fitur belum tersedia.\n";
+            case 8: {
+                cout << "Masukkan Gudang Asal: ";
+                cin >> start;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> end;
+                int result = shortPathSearching(G, start, end);
+                if (result != -1) {
+                    cout << "Jarak terpendek dari " << start << " ke " << end << " adalah " << result << endl;
+                } else {
+                    cout << "Tidak ada jalur yang tersedia dari " << start << " ke " << end << "." << endl;
+                }
                 break;
-            case 7:
+            }
+            case 9: {
+                cout << "Masukkan Gudang Asal: ";
+                cin >> start;
+                cout << "Masukkan Gudang Tujuan: ";
+                cin >> end;
+                int result = longPathSearching(G, start, end);
+                if (result != -1) {
+                    cout << "Jarak terpanjang dari " << start << " ke " << end << " adalah " << endl;
+                } else {
+                    cout << "Tidak ada jalur yang tersedia dari " << start << " ke " << end << "." << endl;
+                }
+                break;
+            }
+            case 10:
                 cout << "Keluar dari program. Terima kasih!\n";
                 break;
             default:
                 cout << "Pilihan tidak valid. Coba lagi.\n";
         }
         cout << endl;
-    } while (choice != 7);
+    } while (choice < 10);
 
     return 0;
 }
